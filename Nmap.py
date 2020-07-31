403c941587edcbfc2f3e1ca1e4b14f92de3be8fd
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2020-07-31 11:57:58 +0200

    Update Nmap.py

--- Nmap.py
+++ Nmap.py
@@ -16,27 +16,27 @@ print("please wait...")
 if scans == '1':
 	scanner.scan(IP, '1-1024', '-v -sS')
 	print(scanner.scaninfo())
-	print("Ip is: " scanner[IP].state())
+	print("Ip is: ", scanner[IP].state())
 	print(scanner[IP].all_protocols())
 	print("open ports: ", scanner[IP]['tcp'].keys())
 
 elif scans == '2':
 	scanner.scan(IP, '1-1024', '-v -O')
 	print(scanner.scaninfo())
-	print("Ip is: " scanner[IP].state())
+	print("Ip is: ", scanner[IP].state())
 	print(scanner[IP].all_protocols())
 	print("open ports: ", scanner[IP]['tcp'].keys())
 
 elif scans  == '3':
 	scanner.scan(IP, '1-1024', '-v -sU')
 	print(scanner.scaninfo())
-	print("Ip is: " scanner[IP].state())
+	print("Ip is: ", scanner[IP].state())
 	print(scanner[IP].all_protocols())
 	print("open ports: ", scanner[IP]['tcp'].keys())
 elif scans == '4':
 	scanner.scan(IP, '1-1024', '-v -sS -sV -sC -A -O')
 	print(scanner.scaninfo())
-	print("Ip is: " scanner[IP].state())
+	print("Ip is: ", scanner[IP].state())
 	print(scanner[IP].all_protocols())
 	print("open ports: ", scanner[IP]['tcp'].keys())
 else scans >= '5'
