78a12ad134e668af68f6177df52c3bc318ce3718
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2020-08-01 12:44:39 +0200

    Add files via upload

---
+++ Qnmap.py
@@ -0,0 +1,72 @@
+import nmap3
+
+nmap = nmap3.Nmap()
+nmapT = nmap3.NmapScanTechniques()
+
+print("""
+                    ___.-------.___
+                _.-' ___.--;--.___ `-._
+             .-' _.-'  /  .+.  \  `-._ `-.
+           .' .-'      |-|-o-|-|      `-. `.
+          (_ <O__      \  `+'  /      __O> _)
+            `--._``-..__`._|_.'__..-''_.--'
+                  ``--._________.--''
+
+  """)
+print(nmap.nmap_version())
+
+print("\n This is an automated nmap scanner created by LittleGoatBenus")
+
+ip_a = input("IP/Host: ")
+
+TypeSc = input("""
+ 1. Top port scan       6. TCP scan
+ 2. OS detection        7. UDP scan
+ 3. Service Version     8. workinprogress
+ 4. ping scan           9. workinprogress
+ 5. SYN ACK scan        10.workinprogress     15. All scan
+
+""")
+
+if TypeSc == '1':
+    res = nmap.scan_top_ports(ip_a)
+    print("please wait...")
+    print(res)
+
+elif TypeSc == '2':
+    osres = nmap.nmap_os_detection(ip_a)
+    print("please wait...")
+    print(osres)
+
+elif TypeSc == 3:
+    serres = nmap.nmap_version_detection(ip_a)
+    print("please wait...")
+    print(serres)
+
+elif TypeSc == 4:
+    pingres = nmapT.nmap_ping_scan(ip_a)
+    print("please wait...")
+    print(pingres)
+
+elif TypeSc == 5:
+    synres = nmapT.nmap_syn_scan(ip_a)
+    print("please wait...")
+    print(synres)
+
+elif TypeSc == 6:
+    tcpres = nmapT.namp_tcp_scan(ip_a)
+    print("please wait...")
+    print(tcpres)
+
+elif TypeSc == 7:
+    udpres = nmapT.nmap_udp_scan(ip_a)
+    print("please wait...")
+    print(udpres)
+
+elif TypeSc == 15:
+    osres = nmap.nmap_os_detection(ip_a)
+    serres = nmap.nmap_version_detection(ip_a)
+    synres = nmapT.nmap_syn_scan(ip_a)
+    Ares = nmap.port_scan(ip_a, args='-Sv -sS -A -O -T4')
+    print("please wait...")
+    print(osres,serres,synres)
